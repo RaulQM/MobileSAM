5ddc4e3ae2293d316e6c2ff54f70cf4c855702c5
raul <r.de.queiroz.mendes@tue.nl>
2025-09-01 14:08:48 +0200

    Adjusted MobileSAM scripts.

---
+++ build/lib/MobileSAMv2/tinyvit/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from perception.MobileSAM.MobileSAMv2.mobilesamv2.build_sam import *
+#from perception.MobileSAM.MobileSAMv2.mobilesamv2.build_sam import (
+#    build_sam_vit_h,
+#    build_sam_vit_l,
+#    build_sam_vit_b,
+#    sam_model_registry,
+#)
+from perception.MobileSAM.MobileSAMv2.mobilesamv2.predictor import SamPredictor
+from perception.MobileSAM.MobileSAMv2.mobilesamv2.automatic_mask_generator import SamAutomaticMaskGenerator
+
